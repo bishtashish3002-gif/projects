f9f368c30c4d7a62b55054326424b95a2ccf223d
Ashish <bishtashish3002@gmail.com>
2026-01-25 20:34:56 -0500

    Fix UI and logic issues in To-Do List app
    Corrected typos and class names in HTML and CSS, improved styling, and fixed JavaScript logic for task handling. Updated the pending tasks section, fixed event handling for adding and deleting tasks, and improved checkbox status toggling. Enhanced overall code consistency and user experience.

--- project_1_To-Do-List/js/script.js
+++ project_1_To-Do-List/js/script.js
@@ -10,8 +10,9 @@ function allTasks(){
 
     //if tasks lenght is 0 then pending num tet content will be no, if not then pending num value will be task's length
     pendingNum.textContent = tasks.length === 0 ? "no": tasks.length;
+
     let allLists= document.querySelectorAll(".list");
-    if(allLists.length>0){
+    if(allLists.length > 0) {
         todoLists.style.marginTop = "20px";
         clearButton.style.pointerEvents = "auto";
         return;
@@ -19,15 +20,17 @@ function allTasks(){
     todoLists.style.marginTop = "0px";
     clearButton.style.pointerEvents = "none";
 }
+
 // add task while we put while in text area and press enter 
-inputField.addEventListener("keyup",(e)=>{
+inputField.addEventListener("keyup",(e) => {
     let inputVal = inputField.value.trim();// trim function remove space of front and back of the inputed value
+    
     // if enter buttion is clicked and inputed value lenght is greated 
     if(e.key ==="Enter" && inputVal.length > 0){
         let liTag =`<li class="list pending" onclick="handleStaturs(this)">
-            <input type ="checkbox"/>
-            <span class="task">${inputVal}</span>
-            <i class="uil uil-trash" onclick="deleteTask(this)"></i>
+                <input type ="checkbox"/>
+                <span class="task">${inputVal}</span>
+                <i class="uil uil-trash" onclick="deleteTask(this)"></i>
             </li>`;
         
         todoLists.insertAdjacentHTML("beforeend", liTag); //inserting li tag inside the todolist div 
@@ -39,10 +42,15 @@ inputField.addEventListener("keyup",(e)=>{
 // checking and unchancking the checkbox while we click on the task
 function handleStaturs(e){
     const checkbox= e.querySelector("input"); // getting checkbox.
+    checkbox.checked = checkbox.checked ? false : true;
     e.classList.toggle("pending");
     allTasks();
 }
+function deleteTask(e){
+    e.parentElsemts.remove();
+    allTasks();
+}
 // deleting all the tasks while we clidck on the clear botton.
-clearButton.addEventListener("click",()=>{
+clearButton.addEventListener("click",() => {
     todoLists.innerHTML = "";
 });
