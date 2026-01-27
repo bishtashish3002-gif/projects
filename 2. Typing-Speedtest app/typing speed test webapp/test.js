fbe353e4c9098b75354fa3ac4292dd99dacfc741
Ashish <bishtashish3002@gmail.com>
2026-01-27 09:43:50 -0500

    Add typing speed test webapp files
    Moved and updated index.html, added style.css and test.js for the typing speed test webapp. Introduced basic UI structure, styling, and initial JavaScript setup. Also added VS Code Live Server port configuration.

---
+++ 2. Typing-Speedtest app/typing speed test webapp/test.js
@@ -0,0 +1,11 @@
+var timer =document.getElementById("timer");
+var timeRemaining =60;
+var testPara = document.getElementById("typepara");
+var textArea = document.getElementById("textArea");
+var startWindow = document.getElementById("startWindow");
+var startButton = document.getElementById("startButton");
+var newTestWindow = document.getElementById("newTestWindow");
+var newtestButton = document.getElementById("newtestButton");
+var score = 0;
+
+var calclate
