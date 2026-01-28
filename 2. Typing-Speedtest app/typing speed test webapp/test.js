c9c73151260b65f65edcb12d12ce74df4cd82b07
Ashish <bishtashish3002@gmail.com>
2026-01-27 21:04:44 -0500

    Add modal styles and implement score calculation
    Added CSS for #startWindow and #newTestWindow modals, including modalBox styling. Implemented a calculateScore function in test.js to count submitted words and log the test paragraph words.

--- 2. Typing-Speedtest app/typing speed test webapp/test.js
+++ 2. Typing-Speedtest app/typing speed test webapp/test.js
@@ -8,4 +8,12 @@ var newTestWindow = document.getElementById("newTestWindow");
 var newtestButton = document.getElementById("newtestButton");
 var score = 0;
 
-var calclate
+var calculateScore = function(){
+    var paraSubmitted = textArea.value;
+    var submittedWorks = paraSubmitted.split(" ");
+    var wordCount = submittedWorks.length;
+
+    var testWords = testPara.innerHTML.split(" ");
+    console.log(testWords);
+    return wordCount;
+};
