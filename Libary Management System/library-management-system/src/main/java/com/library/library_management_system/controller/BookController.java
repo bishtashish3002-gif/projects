b2009fdba67f0549d63958251e57b4c865d6df34
Ashish <bishtashish3002@gmail.com>
2026-07-01 15:36:44 -0500

    Add BookController skeleton and BookService CRUD
    Introduce a BookController REST skeleton with @RestController, @RequestMapping("/books") and an injected BookService field. Implement CRUD operations in BookService: getAllBooks(), getBookById(id), updateBook(id, book) (loads existing, updates fields, saves) and deleteBook(id) (deleteById). Note: controller currently contains only the skeleton and imports; service methods interact with bookRepository and return null when an entity is not found.

--- Libary Management System/library-management-system/src/main/java/com/library/library_management_system/controller/BookController.java
+++ Libary Management System/library-management-system/src/main/java/com/library/library_management_system/controller/BookController.java
@@ -1,5 +1,24 @@
 package com.library.library_management_system.controller;
 
+import java.util.List;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.library.library_management_system.model.Book;
+import com.library.library_management_system.service.BookService;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+      private final BookService bookService;
+
       
 }
--- Libary Management System/library-management-system/src/main/java/com/library/library_management_system/service/BookService.java
+++ Libary Management System/library-management-system/src/main/java/com/library/library_management_system/service/BookService.java
@@ -22,8 +22,29 @@ public class BookService {
       }
 
       // get all books
-      
+      public List<Book> getAllBooks() {
+            return bookRepository.findAll();
+      }
       // get a book by ID
+      public Book getBookById(Long id){
+            return bookRepository.findById(id).orElse(null);
+      }
       // update a book
+      public Book updateBook(Long id, Book updateBook){
+            Book existingBook = bookRepository.findById(id).orElse(null);
+
+            if(existingBook !=null){
+                  existingBook.setTitle(updateBook.getTitle());
+                  existingBook.setAuthor(updateBook.getAuthor());
+                  existingBook.setCategory(updateBook.getCategory());
+                  existingBook.setQuantity(updateBook.getQuantity());
+                  
+                  return bookRepository.save(existingBook);
+            }
+            return null;
+      }
       // delete a book
+      public void deleteBook(Long id){
+            bookRepository.deleteById(id);
+      }
 }
